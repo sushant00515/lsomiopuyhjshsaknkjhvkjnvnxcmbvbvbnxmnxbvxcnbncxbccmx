6ba61180a0499084399996561638154fc3a498f6
sushant00515 <sushantaep01@gmail.com>
2024-12-24 02:34:37 +0545

    Add files via upload

---
+++ fastflow.js
@@ -0,0 +1,31 @@
+let num = document.getElementById('number');
+console.log(num.innerText);
+
+let button = document.getElementById('sendto');
+button.addEventListener('click', () => {
+    let input = document.getElementById('input');
+
+    // Update the number after deducting the value from the input
+    num.innerHTML = num.innerText - input.value;
+
+    let amountValue = input.value;
+    console.log(amountValue);
+
+    let username = document.getElementById('user');
+    let userValue = username.value;
+    console.log(userValue);
+
+    let numValue = num.innerText;
+    localStorage.setItem('amount', amountValue);
+    localStorage.setItem('user', userValue);
+
+    // Clear the input fields
+    input.value = '';
+    let user = document.getElementById('user');
+    user.value = '';
+
+    // Add a 5-second delay before opening the new page
+    setTimeout(() => {
+        window.open("paymentdone.html");
+    }, 10000); // 10000 milliseconds = 10 seconds
+});
